6f5b70815c0119c10a5234627565a5c684a7f34e
archan148 <archanar2909@gmail.com>
2021-03-23 15:03:01 +0530

    source code

---
+++ Doctor_Appointment/signin.php
@@ -0,0 +1,52 @@
+<?php include('header.php'); ?>
+
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">User Login</h3>
+		<div class="formstyles" style="background-color: #101011;color: #0d0623;">
+				<div class="col-md-12">
+
+					<div class="col-md-4"></div>
+					<div class="col-md-4" style="float: right;padding:10px 145px;border: 1px solid lightgrey;margin-right:405px; margin-bottom:30px;background-color:whitesmoke;color:blue;">
+						<input type="button" onclick="location.href='patient_login.php';" value="Patient Login" />
+						<input type="button" onclick="location.href='doctors/doctorlogin.php';" value="Doctor Login" />
+
+					</div>
+					<div class="col-md-4"></div>
+
+
+				</div>
+
+
+
+
+
+    </div><br><br><br><br>
+
+
+
+
+
+
+ <?php include('footer.php'); ?>
+
+
+
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+
+</body>
+</html>
